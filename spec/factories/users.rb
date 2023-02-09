64fdb8ab4f21ebf20e9a797dbd62139319f7c9b7
developer <himanshu60.k@gmail.com>
2023-02-09 17:11:38 +0530

    used factorybot for user rspec

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    name { SecureRandom.base64(5) }
+    email { "#{SecureRandom.uuid}@gmail.com" }
+    password { SecureRandom.hex(3) }
+    DOB { Date.today }
+    gender { ['male', 'female'].sample }
+    role { ['buyer', 'seller'].sample }
+    contact { "#{SecureRandom.random_number(9e9).to_i}" }
+  end
+end
